b95c704b1e64f4cd9e9c4c08d82fd5f3f675c633
CrispusDH <qacrispus@gmail.com>
2019-08-01 17:16:42 +0300

    implement remove

--- src/component/item.tsx
+++ src/component/item.tsx
@@ -1,9 +1,28 @@
-import * as React from 'react'
-import { ToDo } from "./form";
+import React from 'react';
+import { ToDo } from './form';
+import { FilterOptions } from './list';
 
-export function Item(todo: ToDo) {
-  return <li
-    key={todo.id}
-    onClick={() => todo.toggleComplete()}
-  >{JSON.stringify(todo.isComplete)}</li>
+interface Props {
+  todo: ToDo,
+  filter: FilterOptions,
+  handleDeleteToDo: (id: string) => void
+}
+
+export function Item(props: Props) {
+  return (
+    <li
+      style={{
+        display: "flex",
+        justifyContent: "start",
+        textDecorationLine: props.todo.isComplete && props.filter !== FilterOptions.Completed ? 'line-through' : ''
+      }}
+      key={props.todo.id}
+      onClick={() => props.todo.toggleComplete()}
+    >{props.todo.text}
+    <button onClick={(event) => {
+      event.stopPropagation();
+      props.handleDeleteToDo(props.todo.id)
+    }}>x</button>
+    </li>
+  )
 }
--- src/component/list.tsx
+++ src/component/list.tsx
@@ -1,30 +1,71 @@
-import * as React from 'react';
-import {useEffect, useState} from 'react'
+import React, { useState } from 'react';
 import { Form, ToDo } from './form';
 import { Item } from './item';
 
 export function List() {
-  const [todos, setTodos] = useState<ToDo[]>([]);
+  const [todos, setToDos] = useState<ToDo[]>([]);
+  const [filter, setFilter] = useState<FilterOptions>(FilterOptions.All);
 
   const onSubmit = (todo: ToDo) => {
-    setTodos([todo, ...todos]);
+    setToDos([todo, ...todos]);
   };
 
   const onClick = () => {
-    setTodos([...todos]);
+    setToDos([...todos]);
   };
 
-  const show = () => {
+  const show = (todos: ToDo[]) => {
     const list = todos.map((todo) => {
-      return Item(todo)
+      return Item({ todo, filter, handleDeleteToDo})
     });
     return <ul onClick={onClick}>{list}</ul>;
+  };
+
+  const left = () => {
+    return todos.filter((todo) => !todo.isComplete).length;
+  };
+
+  const filtered = () => {
+    let array: ToDo[] = [];
+    switch (filter) {
+      case FilterOptions.All: {
+        array = todos;
+        break;
+      }
+      case FilterOptions.Active: {
+        array = todos.filter((todo) => !todo.isComplete);
+        break;
+      }
+      case FilterOptions.Completed: {
+        array = todos.filter((todo) => todo.isComplete);
+        break;
+      }
+    }
+    return array;
+  };
+
+  const handleDeleteToDo = (id: string) => {
+    setToDos(todos.filter((todo) => {
+      return todo.id !== id;
+    }));
   };
 
   return (
     <div>
       <Form onSubmit={onSubmit}/>
-      {show()}
+      {show(filtered())}
+      <label>todos left: {left()}</label>
+      <div>
+        <button onClick={() => setFilter(FilterOptions.All)}>all</button>
+        <button onClick={() => setFilter(FilterOptions.Active)}>active</button>
+        <button onClick={() => setFilter(FilterOptions.Completed)}>completed</button>
+      </div>
     </div>
   )
+}
+
+export enum FilterOptions {
+  All,
+  Active,
+  Completed
 }
